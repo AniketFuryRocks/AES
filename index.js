cc5eff5451acf3b641d9c84bc74f382e44a3e7a0
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-02-03 09:53:55 +0530

    HEX to hex (bug) fized

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var lse = require('./LSEncryption');
 var aes = require("aes-js");
+var { base64decode, base64encode } = require("nodejs-base64")
+
+//express js
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+//req body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//static routing
 var assets = __dirname + "/assets";
 
 app.get('/', function (req, res) {
@@ -21,25 +27,35 @@ var server = app.listen(8081, function () {
     console.log("AES Server listening at http://localhost:%s", port)
 })
 
-//post
+//route /decrypt POST Method
 app.post('/decrypt', function (req, res) {
     let key = lse.getKey(req.body.password, req.body.salt, req.body['key-length']);
     //convert text to bytes
-    if (req.body['text-format'] == "hex")
-        req.body.text = aes.utils.hex.toBytes(req.body.text);
-    else
-        req.body.text = aes.utils.utf8.toBytes(req.body.text);
-    let decrypt = lse.decrypt(key, req.body.text);
-    res.send("HEX : " + aes.utils.hex.fromBytes(decrypt) + "<br>TEXT : " + lse.decToText(decrypt) + "<br>BYTES : " + decrypt);
+    req.body.text = convertToBytes(req.body.text,req.body['text-format']);//reuse req.body.text variable
+    res.send(toString(lse.decrypt(key, req.body.text)));//decrypt text using key and pass it to toString() which gives a meaning full return (JSON)
 })
 
+// route /encrypt POST Method
 app.post('/encrypt', function (req, res) {
     let key = lse.getKey(req.body.password, req.body.salt, req.body['key-length']);
     //convert text to bytes
-    if (req.body['text-format'] == "hex")
-        req.body.text = aes.utils.hex.toBytes(req.body.text);
-    else
-        req.body.text = aes.utils.utf8.toBytes(req.body.text);
-    let encrypt = lse.encrypt(key, req.body.text);
-    res.send("HEX : " + aes.utils.hex.fromBytes(encrypt) + "<br>TEXT : " + lse.decToText(encrypt) + "<br>BYTES : " + encrypt);
+    req.body.text = convertToBytes(req.body.text,req.body['text-format'])
+    res.send(toString(lse.encrypt(key, req.body.text)));//decrypt text using key and pass it to toString() which gives a meaning full return (JSON)
 })
+
+function convertToBytes(text, format) {
+    if (format == 'base64')//BASE64
+        return aes.utils.utf8.toBytes(base64decode(text));
+    else if (format == "hex")//BASE16
+        return aes.utils.hex.toBytes(text);
+    else//Assuming it to be utf8
+        return aes.utils.utf8.toBytes(text);
+}
+
+function toString(bytes) {
+    return JSON.stringify({
+        HEX : aes.utils.hex.fromBytes(bytes),
+        'UTF-8' : aes.utils.utf8.fromBytes(bytes),
+        BASE64 : base64encode(this['UTF-8'])
+    })
+}
