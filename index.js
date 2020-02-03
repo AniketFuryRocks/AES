7a89321d5d19701d809906ba5faeb936124f165e
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-02-03 09:36:18 +0530

    HEX to hex (bug) fized

--- index.js
+++ index.js
@@ -22,30 +22,24 @@ var server = app.listen(8081, function () {
 })
 
 //post
-app.post('/decrypt',function (req,res) {
-    let key = lse.getKey(req.body.password,req.body.salt,req.body['key-length']);
+app.post('/decrypt', function (req, res) {
+    let key = lse.getKey(req.body.password, req.body.salt, req.body['key-length']);
     //convert text to bytes
-    if(req.body['text-format'] == "HEX")
+    if (req.body['text-format'] == "hex")
         req.body.text = aes.utils.hex.toBytes(req.body.text);
     else
         req.body.text = aes.utils.utf8.toBytes(req.body.text);
-    let decrypt = lse.decrypt(key,req.body.text);
-    res.send("HEX : " + aes.utils.hex.fromBytes(decrypt) + "<br>TEXT : "+lse.decToText(decrypt) + "<br>BYTES : "+decrypt);
-})
-app.post('/encrypt',function (req,res) {
-    let key = lse.getKey(req.body.password,req.body.salt,req.body['key-length']);
-    //convert text to bytes
-    if(req.body['text-format'] == "HEX")
-        req.body.text = aes.utils.hex.toBytes(req.body.text);
-    else
-        req.body.text = aes.utils.utf8.toBytes(req.body.text);
-    let encrypt = lse.encrypt(key,req.body.text);
-    res.send("HEX : " + aes.utils.hex.fromBytes(encrypt) + "<br>TEXT : "+lse.decToText(encrypt) + "<br>BYTES : "+encrypt);
+    let decrypt = lse.decrypt(key, req.body.text);
+    res.send("HEX : " + aes.utils.hex.fromBytes(decrypt) + "<br>TEXT : " + lse.decToText(decrypt) + "<br>BYTES : " + decrypt);
 })
 
-//eg
-var key = lse.getKey("eadded","1234",128);
-var enc = lse.encrypt(key,aes.utils.utf8.toBytes("1234567890123456"));
-var hex = aes.utils.hex.fromBytes(enc);
-console.log("Enc hex = " + hex);
-console.log("Decrypt = " + aes.utils.utf8.fromBytes(lse.decrypt(key,aes.utils.hex.toBytes(hex))));
+app.post('/encrypt', function (req, res) {
+    let key = lse.getKey(req.body.password, req.body.salt, req.body['key-length']);
+    //convert text to bytes
+    if (req.body['text-format'] == "hex")
+        req.body.text = aes.utils.hex.toBytes(req.body.text);
+    else
+        req.body.text = aes.utils.utf8.toBytes(req.body.text);
+    let encrypt = lse.encrypt(key, req.body.text);
+    res.send("HEX : " + aes.utils.hex.fromBytes(encrypt) + "<br>TEXT : " + lse.decToText(encrypt) + "<br>BYTES : " + encrypt);
+})
